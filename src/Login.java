99b38b50ed66aa6627cda0b67f145f9774ba3e9f
Sang140790 <105843692+Sang140790@users.noreply.github.com>
2023-06-06 18:32:29 +0530

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
 
 import javax.servlet.http.HttpServletRequest;
 
-// Class Declaration
+// Class DeclarationS
 class Login
 {
     public static void main(String[] args)   
